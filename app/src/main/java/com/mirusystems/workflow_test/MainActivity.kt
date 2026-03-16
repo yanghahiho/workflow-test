b673ff1277b783d8cdeedb96145a017f993b5eb0
Hawon Yang <hw-yang@mirusystems.com>
2026-03-16 09:59:30 +0900

    DOC: Add README.md

--- app/src/main/java/com/mirusystems/workflow_test/MainActivity.kt
+++ app/src/main/java/com/mirusystems/workflow_test/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!da", modifier = modifier
+        text = "Hello $name!fda", modifier = modifier
     )
 }
 
